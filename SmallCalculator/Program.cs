445bef16dd63a38c6a475c9017089a04af2b6868
scarlettlemon <scarlettlemon4@gmail.com>
2026-02-28 21:18:19 +0000

    Update Program.cs

--- SmallCalculator/Program.cs
+++ SmallCalculator/Program.cs
@@ -1,3 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Try")
+﻿using System;
+
+namespace SmallCalculator
+{
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // while running boolean to break 
+            // display choices 
+            // input of what function to use 
+            // run associated function 
+
+            // required functions 
+            // add 
+            // subtract 
+            // multiply 
+            // divide 
+            // Leave game - "q"
+            // Invalid choice - check input
+
+            DisplayChoices();
+
+
+        private static void DisplayChoices()
+        {
+            Console.WriteLine(" ____ Welcome to the Simple Calculator ____\n");
+            Console.WriteLine("You have the following choices:")
+            Console.WriteLine("To add - enter 1");
+            Console.WriteLine("To subtract - enter 2");
+            Console.WriteLine("To multiply - enter 3");
+            Console.WriteLine("To divide - enter 4");
+            Console.WriteLine("To leave the program, enter \"q\"");
+        }
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
